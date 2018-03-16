decbba5440941af3598d45b325e17012ce23da06
Admir Tiric <Admirtiric25@Hotmail.com>
2018-03-16 02:30:23 -0700

    '1.1'

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(express.static(path.resolve('./dist/assets')));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 
 
